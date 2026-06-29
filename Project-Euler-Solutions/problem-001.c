ee1dbd3869eb2fcf1031115790bb907bef8605a4
Ahmet-Ucan <aucan2022@gmail.com>
2026-06-29 15:34:03 +0300

    Implement solution for Project Euler problem 001
    Calculate and display multiples of 3 or 5 below 1000.

--- Project-Euler-Solutions/problem-001.c
+++ Project-Euler-Solutions/problem-001.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int sub = 0;
+	int i;
+	int j = 1;
+	
+	for(i = 0; i < 1001; i++){
+		if((i % 3 == 0) || (i % 5 == 0)){
+			printf("%dst multiple = %d\n", j , i);
+			sub = sub + i;
+			j++;	
+			}
+			
+		}
+	printf("\nthe num of all the multiples of 3 or 5 below 1000 = %d", j-1);	
+	printf("\nthe sum of all the multiples of 3 or 5 below 1000 = %d", sub);
+	return 0;
+}
+
 
